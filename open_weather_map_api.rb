d58f20155443a53efede99e8f20cd01baed40f91
Grzegorz Dziedzic <grzegorzdziedzic@onet.pl>
2019-05-10 20:23:55 +0200

    added pony gem for mail sending and little translation for weather description

--- open_weather_map_api.rb
+++ open_weather_map_api.rb
@@ -1,10 +1,37 @@
 require "faraday"
 require "awesome_print"
 require "json"
+require "pony"
+require "easy_translate"
 
 def f_to_c f
   celsius = (f - 32)/1.8
   return celsius.round(1)
+end
+
+def translation word
+  case word
+  when "shower rain"
+    "Mżawka"
+  when "clear sky"
+    "Bezchmurnie"
+  when "few clouds"
+    "Małe zachmurzenie"
+  when "scattered clouds"
+    "Zachmurzenie"
+  when "broken clouds"
+    "Załamanie chmury"
+  when "rain"
+    "Deszcz"
+  when "thunderstorm"
+    "Burza"
+  when "snow"
+    "Śnieg"
+  when "mist"
+    "Mgła"
+  else 
+    word
+  end
 end
 
 api_key = "cbe7de6d147912e998501c064c7d1dc4"
@@ -23,6 +50,8 @@ main = json["main"]
 
 name = json["name"]
 weather_description = json["weather"].first["description"]
+icon = json["weather"].first["icon"]
+icon_src = "http://openweathermap.org/img/w/" + icon + ".png"
 min_temp = f_to_c(main["temp_min"])
 max_temp = f_to_c(main["temp_max"])
 temp = f_to_c(main["temp"])
@@ -30,5 +59,33 @@ pressure = main["pressure"]
 humidity = main["humidity"]
 wind_speed = json["wind"]["speed"]
 clouds = json["clouds"]["all"]
+time = Time.new
+current_time = "#{time.hour}:#{time.min} #{time.day}.#{time.month}.#{time.year}"
 
-puts "Pogoda dla miasta #{name}\nOpady: #{weather_description}\nTemperatura: #{temp} stopni Celcjusza(od #{min_temp} do #{max_temp})\nCisnienie: #{pressure}hpa\nPredkość wiatru: #{wind_speed}mph\nWilgotność powietrza: #{humidity}%\nZachmurzenie: #{clouds}%"
+message_html = "<h2 style='text-align: center'>Pogoda dla miasta #{name}</h2><br><img src=#{icon_src} width='50' height ='50'><p>Opady: #{translation(weather_description)}</p><p>Temperatura: #{temp} stopni Celsjusza(od #{min_temp} do #{max_temp})</p><p>Cisnienie: #{pressure}hpa</p><p>Predkość wiatru: #{wind_speed}mph</p><p>Wilgotność powietrza: #{humidity}%</p><p>Zachmurzenie: #{clouds}%</p>"
+
+Pony.mail({
+  :to => 'grzeluud@gmail.com',
+  :subject => 'Pogoda ' + current_time ,
+  :html_body => 
+    '<header>
+      <h1 style="text-align: center">GrzeluBot v1.0</h1>
+     </header>
+     <main style="line-height: 1.5">
+        ' + message_html + '
+     </main>
+     <footer style="text-align: center; box-sizing: border-box; padding-top: 32px;" >
+      Wiadomość wygenerowana przez GrzeluBot, Pozdrawiam
+     </footer>',
+  :via => :smtp,
+  :charset => "UTF-8",
+  :via_options => {
+    :address              => 'smtp.gmail.com',
+    :port                 => '587',
+    :enable_starttls_auto => true,
+    :user_name            => 'grzelubot@gmail.com',
+    :password             => 'Grzelubot1@3',
+    :authentication       => :plain, 
+    :domain               => "localhost.localdomain"
+  }
+})
